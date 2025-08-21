9f58f239b01c06ce22bb0b13d5b01c3fc7e36801
Isaac-Subirana <isaacsubiranac@gmail.com>
2025-08-21 08:34:12 +0200

    Version 0.0. Does nothing on its own appart from calling the system shell.

---
+++ v0.0/main.py
@@ -0,0 +1,27 @@
+from subprocess import run as subprocess_run
+
+command = ""
+
+def run():
+    command = input("\n> ")
+    isnotexit(command)
+    try:
+        subprocess_run(command)
+        run()
+        command = input("\n> ")
+    except FileNotFoundError:
+        print("Error: Your system does not recognize the command you tried to run.")
+        run()
+        command = input("\n> ")
+
+def isnotexit(command):
+    if command.lower() == "exit":
+        exit()
+
+
+def main():
+    print("Basic shell in Python, designed for all Operating Systems.\nVersion 0.0")
+    #It's version 0.0 because it does nothing on its own and just calls the system shell for all commands.
+    run()
+
+main()
